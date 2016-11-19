ca56e49bdb1c53675395c391f7f002471e96875e
vogdev <vogdev@gmail.com>
2016-11-19 01:27:14 +0000

    Added bootsrap

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'geocoder'
 gem 'omniauth-facebook'
 gem 'omniauth-github'
 gem "figaro"
+gem 'bootstrap-sass', '~> 3.3.6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :votes
   acts_as_voter
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def self.from_omniauth(auth)
