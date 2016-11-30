9185fd0a814b43f2eba38950aa7872510897fb56
Soufiane akki <gamingblood@outlook.fr>
2016-11-30 23:16:02 +0000

    now the admin can create new foodspots remotely via ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap-sprockets
 //= require bootstrap-material-design
 //= require jquery_ujs
+//= require data-confirm-modal
 //= require turbolinks
 //= require_tree .
 $(function () {
